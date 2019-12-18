3d9d1fcaf3c93682df2ff46c4dc99bb4d7244538
jpz95 <jpzambrano95@hotmail.com>
2019-12-18 01:10:17 -0600

    Added normalize.css as vendor style
     - might want to do it through npm dependency
     - deleted index.css because it was unused

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './styles/main.styl'
+import './styles/main.styl';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
