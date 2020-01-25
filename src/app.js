e9580c3752afd6d08f32de900e004f4a58683335
jpz95 <jpzambrano95@hotmail.com>
2020-01-25 17:44:14 -0600

    Convert App CSS to Stylus

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './app.css';
 import SearchBar from './components/search-bar/search-bar';
 import Grid from './components/grid/grid';
 // import logo from './logo.svg';
