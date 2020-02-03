e76e6fed8f6bada1bf637aa1233fedeab3d0e687
jpz95 <jpzambrano95@hotmail.com>
2020-02-03 14:26:52 -0600

    Rewrites grid 'header' creation
     - Moved header creation to the grid level, so we don't have to pass down
        variables too much.

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ import React from 'react';
 import SearchBar from './components/search-bar/search-bar';
 import Grid from './components/grid/grid';
 
-function App() {
+const App = () => {
+  const columns = [ "cell1", "cell2", "cell3" ];
+
   return (
     <div className="app">
+
       <header className="app__header">
         <SearchBar></SearchBar>
       </header>
+
       <div className="app__content">
         <div className="playlist__header">umpah umpah</div>
-        <Grid>{{ columnNames: [ "cell1", "cell2", "cell3" ] }}</Grid>
+        <Grid columns={columns}></Grid>
       </div>
+
     </div>
   );
 }
--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,33 +1,19 @@
-import React, { useState } from 'react';
-import GridRow from './grid-row';
+import React from 'react';
 
-function Grid(props) {
-    const [columnNames] = useState(props.children.columnNames);
-
-    const render = () => {
-        return renderHeaderRow(); //.concat(this.renderRows());
-    };
-
-    const renderHeaderRow = () => {
-        return (
-            <GridRow className={`-header`}>{columnNames}</GridRow>
-        );
-    };
-
-    /* const renderRows = () => {
-        let children = React.Children.map(this.props.children, child => {
-            console.log("making child", child)
-            return (
-                <GridRow>{child}</GridRow>
-            );
-        });
-        console.log(children);
-        return children;
-    }; */
-
+const Grid = ({ columns = [] }) => {
     return (
         <div className="grid">
-            { render() }
+
+            <div className="grid__row -header">
+                {columns.map(column => (
+                    <div key={`header-${column}`}
+                        className="grid__cell"
+                    >
+                        {column}
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 };
