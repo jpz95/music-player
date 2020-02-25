e886c77f2318ef22014140203000005e8cf567e8
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-02-24 23:56:09 -0600

    Moves 'playlist' code to its own file

--- src/app.js
+++ src/app.js
@@ -1,51 +1,18 @@
-import React, { /* useState, useEffect */ } from 'react';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import React from 'react';
 
 import SearchBar from '@/components/search-bar/search-bar';
-import Grid from '@/components/grid/grid';
-
-import { firestore } from './base';
-
-/* const PlaylistDispatch = React.createContext(null);
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'getDataFromStore':
-            return useCollectionData('music', { idField: 'id' });
-
-        default:
-            break;
-    }
-}; */
+import Playlist from './features/playlist';
 
 const App = () => {
-    // fetching 'latest playlist' view (not really but y'know)
-    const columns = {
-        song: { label: "Song" },
-        artist: { label: "Artist" },
-        length: { label: "Length" },
-    };
-
-    const [playlistSongs, ] = useCollectionData(
-        firestore
-            .collection("playlists")
-            .doc("greatest_playlist")
-            .collection("music"),
-        { idField: "id" }
-    );
-
     return (
         <div className="app">
 
-        <header className="app__header">
+            <header className="app__header">
                 <SearchBar></SearchBar>
             </header>
 
             <div className="app__content">
-                <div className="playlist__header">the greatest playlist :dab:</div>
-                <Grid
-                    columns={columns}
-                    data={playlistSongs || []}
-                ></Grid>
+                <Playlist></Playlist>
             </div>
         </div>
     );
---
+++ src/features/playlist.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import Grid from '@/components/grid/grid';
+
+import { firestore } from '@/base';
+
+const Playlist = (props) => {
+    const { playlistName = "greatest_playlist" } = props;
+
+    // fetching 'latest playlist' view (not really but y'know)
+    const columns = {
+        song: { label: "Song" },
+        artist: { label: "Artist" },
+        length: { label: "Length" },
+    };
+
+    const [playlistSongs, ] = useCollectionData(
+        firestore
+            .collection("playlists")
+            .doc(playlistName)
+            .collection("music"),
+        { idField: "id" }
+    );
+
+    return (
+        <div>
+            <div className="playlist__header">the greatest playlist :dab:</div>
+            <Grid
+                columns={columns}
+                data={playlistSongs || []}
+            ></Grid>
+        </div>
+    );
+}
+
+export default Playlist;
