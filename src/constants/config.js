54e4c439e631ea000ed08ba98742ea6b8a6fba58
jpz95 <jpzambrano95@hotmail.com>
2020-03-05 16:13:04 -0600

    Addresses leaking API key
     - Firebase key was made public by previous architecture. It was refactored
        using dotenv. The API key is now stored locally on each machine, in
        a '.env.local' file
    
     - Configs will have '-config' appended to them.

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,11 @@
-import firebase from './_configs/firebase';
+import app from './_configs/app-config';
+import firebase from './_configs/firebase-config';
+
+const config = Object.assign({}, firebase, app);
+export default config;
+
+const appValues = app['app'];
+export { appValues as appConfig };
 
 const firebaseValues = firebase['firebase'];
 export { firebaseValues as firebaseConfig };
-
-const config = Object.assign({}, firebase);
-export default config;
