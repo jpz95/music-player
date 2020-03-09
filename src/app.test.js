fead061b5d3b2eaf55eba2fff2bd26546980ea01
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-03-09 02:22:43 -0500

    Helps Jest find js modules
     - Added 'src' folder to Jests module paths.
     - Updates packages with new patches.

--- src/app.test.js
+++ src/app.test.js
@@ -4,6 +4,7 @@ import App from './app';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // TODO create shallow render (with enzyme?)
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
