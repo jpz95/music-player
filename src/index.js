d412ada9ace66fb7cd2bfcdcf34e6cd77f421d63
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-03-18 23:28:18 -0500

    Adds rest of essential 'Noto-Sans' font
     - Adds italic, bold, and bold+italic.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import ReactDOM from 'react-dom';
 import Router from 'components/router/router';
 
 // Load fonts into bundle
+// TODO move into 'font-loader' module
 import 'fonts/noto-sans/NotoSans-Regular.eot';
 import 'fonts/noto-sans/NotoSans-Regular.ttf';
 import 'fonts/noto-sans/NotoSans-Regular.woff';
 import 'fonts/noto-sans/NotoSans-Regular.woff2';
+import 'fonts/noto-sans/NotoSans-Italic.eot';
+import 'fonts/noto-sans/NotoSans-Italic.ttf';
+import 'fonts/noto-sans/NotoSans-Italic.woff';
+import 'fonts/noto-sans/NotoSans-Italic.woff2';
+import 'fonts/noto-sans/NotoSans-Bold.eot';
+import 'fonts/noto-sans/NotoSans-Bold.ttf';
+import 'fonts/noto-sans/NotoSans-Bold.woff';
+import 'fonts/noto-sans/NotoSans-Bold.woff2';
+import 'fonts/noto-sans/NotoSans-BoldItalic.eot';
+import 'fonts/noto-sans/NotoSans-BoldItalic.ttf';
+import 'fonts/noto-sans/NotoSans-BoldItalic.woff';
+import 'fonts/noto-sans/NotoSans-BoldItalic.woff2';
 
 import 'styles/main.scss';
 
