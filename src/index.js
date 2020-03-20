85b8a15ae1a44bcf78a0ec5018e164b54a645867
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-03-19 20:41:02 -0500

    Renames Noto Sans fontface
     - Removed an excess mixin by normalizing how the font is named.

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ import Router from 'components/router/router';
 
 // Load fonts into bundle
 // TODO move into 'font-loader' module
-import 'fonts/noto-sans/NotoSans-Regular.eot';
-import 'fonts/noto-sans/NotoSans-Regular.ttf';
-import 'fonts/noto-sans/NotoSans-Regular.woff';
-import 'fonts/noto-sans/NotoSans-Regular.woff2';
-import 'fonts/noto-sans/NotoSans-Italic.eot';
-import 'fonts/noto-sans/NotoSans-Italic.ttf';
-import 'fonts/noto-sans/NotoSans-Italic.woff';
-import 'fonts/noto-sans/NotoSans-Italic.woff2';
-import 'fonts/noto-sans/NotoSans-Bold.eot';
-import 'fonts/noto-sans/NotoSans-Bold.ttf';
-import 'fonts/noto-sans/NotoSans-Bold.woff';
-import 'fonts/noto-sans/NotoSans-Bold.woff2';
-import 'fonts/noto-sans/NotoSans-BoldItalic.eot';
-import 'fonts/noto-sans/NotoSans-BoldItalic.ttf';
-import 'fonts/noto-sans/NotoSans-BoldItalic.woff';
-import 'fonts/noto-sans/NotoSans-BoldItalic.woff2';
+import 'fonts/noto-sans/noto-sans-regular.eot';
+import 'fonts/noto-sans/noto-sans-regular.ttf';
+import 'fonts/noto-sans/noto-sans-regular.woff';
+import 'fonts/noto-sans/noto-sans-regular.woff2';
+import 'fonts/noto-sans/noto-sans-italic.eot';
+import 'fonts/noto-sans/noto-sans-italic.ttf';
+import 'fonts/noto-sans/noto-sans-italic.woff';
+import 'fonts/noto-sans/noto-sans-italic.woff2';
+import 'fonts/noto-sans/noto-sans-bold.eot';
+import 'fonts/noto-sans/noto-sans-bold.ttf';
+import 'fonts/noto-sans/noto-sans-bold.woff';
+import 'fonts/noto-sans/noto-sans-bold.woff2';
+import 'fonts/noto-sans/noto-sans-bold-italic.eot';
+import 'fonts/noto-sans/noto-sans-bold-italic.ttf';
+import 'fonts/noto-sans/noto-sans-bold-italic.woff';
+import 'fonts/noto-sans/noto-sans-bold-italic.woff2';
 
 import 'styles/main.scss';
 
