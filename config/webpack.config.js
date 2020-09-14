55bb19dc17dd23f7529182d03b70fd8054fb3ffe
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-09-14 18:25:08 -0500

    Updates split chunks options
     - Fixes names for npm packages. Uses nameForCondition because
        it is what webpack uses internally for npm package names.
    
     - Adds default cache groups, 'vendors' and 'default'.
     - Adds custom options and comments.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -273,7 +273,48 @@ module.exports = function(webpackEnv) {
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
         chunks: 'all',
+        maxInitialRequests: Infinity,
+        // Splits chunks if it exceeds this threshold.
+        minSize: 30000, // 30 kb
+        automaticNameDelimiter: '-',
+        // TODO report bug for html-webpack-plugin.
+        // plugin defines 'vendors' chunk's name as undefined, filtering it out
+        // when determining which chunks to insert as scripts into html.
         name: false,
+        cacheGroups: {
+          // Creates a named vendor chunk, if its greater than the minSize threshold.
+          namedVendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              if (!module.nameForCondition) {
+                return module.identifier();
+              }
+
+              // Get the name. E.g. node_modules/packageName/not/this/part.js
+              //  or node_modules/packageName
+              const matches = module
+                .nameForCondition()
+                .match(/[\\/]node_modules[\\/](.*?)([\\/|$])/) || [];
+
+              const packageName = matches.length > 0 ? matches[1] : module.identifier();
+
+              // npm package names are URL-safe, but some servers
+              // don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`;
+            },
+          },
+          // Creates a generic vendors chunk (default group).
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            name: 'vendors',
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          },
+        },
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
